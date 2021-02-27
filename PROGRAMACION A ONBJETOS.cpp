bbdad9a5d18269bab832dfb33e8f233a1e3aa178
radilla0504 <44661741+radilla0504@users.noreply.github.com>
2021-02-26 18:48:55 -0600

    Add files via upload
    PRIMER TAREA

---
+++ PROGRAMACION A ONBJETOS.cpp
@@ -0,0 +1,15 @@
+/*
+*PRIMER TAREA PROGRMACION
+*KEVIN RADILLA SANCHEZ
+*PROGRAMA DE REPASO
+*/
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	printf("--PROGRAMA QUE IMPRIME MI NOMBRE--");
+	printf("\n\nHOLA MI NOMBRE ES KEVIN RADILLA SANCHEZ :) ");
+	getch();
+	return 0;
+}
